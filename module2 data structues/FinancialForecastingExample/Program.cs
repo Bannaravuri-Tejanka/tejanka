a04885f4f79a9d5a0912e4bf0c133d1a4f475953
Bannaravuri-Tejanka <231fa05100@gmail.com>
2026-07-10 16:34:29 +0530

    Added Ecommerce Search and Financial Forecasting examples

---
+++ module2 data structues/FinancialForecastingExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static double Forecast(double amount, double rate, int years)
+    {
+        if (years == 0)
+        {
+            return amount;
+        }
+        return Forecast(amount * (1 + rate), rate, years - 1);
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("Enter Initial Amount: ");
+        double amount = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter Growth Rate (Example 0.10): ");
+        double rate = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter Number of Years: ");
+        int years = Convert.ToInt32(Console.ReadLine());
+        double futureValue = Forecast(amount, rate, years);
+
+        Console.WriteLine("Future Value = " + futureValue);
+    }
+}
+
