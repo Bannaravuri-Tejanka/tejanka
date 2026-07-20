91951b16e7f990499b9df2932ca755f0689abd4f
Bannaravuri-Tejanka <231fa05100@gmail.com>
2026-07-20 08:42:36 +0530

    Added Advanced SQL exercises and project workspace

--- module2 data structues/EcommerceSearchExample/obj/Debug/net10.0/EcommerceSearchExample.AssemblyInfo.cs
+++ module2 data structues/EcommerceSearchExample/obj/Debug/net10.0/EcommerceSearchExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceSearchExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b02c862b4717141ae0372a6609e57a0361b21ef7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a04885f4f79a9d5a0912e4bf0c133d1a4f475953")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceSearchExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceSearchExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
